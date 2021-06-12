b880036e1246f9d74b9a76e929a86d9ba4f94e89
StanTheMan7 <sts.graur@gmail.com>
2021-06-12 20:15:47 +0200

    calculetre finit

--- src/js/elements.js
+++ src/js/elements.js
@@ -5,37 +5,92 @@
 
 /* // todo --------------------------mes elements html -------------------------- */
 
-let myBody = document.querySelector('body');
-let mySection =  document.createElement('section');
-let myDivContainer = document.createElement('div');
-let myDivDisplay = document.createElement('div');
-let toutLesBoutons = document.createElement('div');
-let myDisplay = document.createElement('div')
+let monBody = document.body;
+let mySection = document.createElement('section');
+monBody.appendChild(mySection)
+
+let divContainer = document.createElement('div');
+divContainer.setAttribute("class", 'divContainer');
+monBody.appendChild(divContainer);
+mySection.appendChild(divContainer);
 
 
-myBody.appendChild(mySection)
-mySection.appendChild(myDivContainer)
-myDivContainer.appendChild(myDivDisplay)
+let divDisplay = document.createElement('div');
+divDisplay.setAttribute('id','divDisplay');
+divContainer.appendChild(divDisplay);
+
+let divBtn = document.createElement('div');
+divBtn.setAttribute('class','divBtns');
+divContainer.appendChild(divBtn);
 
 
-let tab = ["1","2","3","4","5","6","7","8","9","0","+","-","/","*","C","="]
 
 
+let tab = ["1","2","3","*","4","5","6","+","7","8","9","-","C",".","=","/"]
+let i = 0;
 tab.forEach(element => {
-    let button = document.createElement('button')
-    button.innerHTML = element
-    myDivContainer.appendChild(button)
-    button.setAttribute("class", 'buttons')
+    element = document.createElement('button')
+    element.innerText = tab[i]
+    divBtn.appendChild(element)
+    element.setAttribute('class', 'mesBoutons')
+    i++;
+
 });
 
+
+let btn1 = document.querySelectorAll('button')[14]
+btn1.setAttribute('id', 'equal')
+
+
+
+let allButtons = Array.from(document.getElementsByClassName("mesBoutons"));
+
+allButtons.forEach(element => {
+    element.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'C':
+                divDisplay.innerText ='';
+                break;
+                case'.': 
+                if(e.target.innerText == '.'){
+                    divDisplay.innerText = 'error404'
+                    alert('point is not available');
+                }
+                break;
+                case '=' :
+                    try{
+                        divDisplay.innerText =eval(divDisplay.innerText);
+                    } catch {
+                        divDisplay.innerText ='Error!'
+                    }
+                    break;
+            default: 
+            divDisplay.innerText += e.target.innerText;
+        }
+    })
+})
 /* ----------------------------- // appendChild ----------------------------- */
+
+// myBody.appendChild(mySection)
+// mySection.appendChild(myDivContainer)
+// myDivContainer.appendChild(toutLesBoutons)
+// myDivContainer.appendChild(myDivDisplay)
+// myDivDisplay.appendChild(display1)
+// myDivDisplay.appendChild(display2)
+// myDivDisplay.appendChild(tempResult)
+
+// let tab = ["1","2","3","4","5","6","7","8","9","0","+","-","/","*","C","="]
+
+// let button ;
+// toutLesBoutons.forEach(element =>{
+//     element.innerHTML == '1' || '2' || '3'|| '4'|| '5'|| '6'|| "7"|| '8'|| '9'|| '0'){
+//         button.classList('class', 'numbers')
+
+// })
+
+
 
 
 
 
 /* --------------------------- // to set attribute -------------------------- */
-
-
-
-
-console.log(myBody);
